3f2deecf8e2d41b9ab46ec0013581ff44c904bf0
Persifer <giorgino.antonio32@gmail.com>
2023-10-19 16:16:46 +0200

    Setup for chart drawing

--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,30 @@
+using System.Windows.Forms;
+
 namespace FormForExcercise
+
 {
     public partial class Form1 : Form
     {
+
+        private Point mouseDownLocation;
+        private bool isDragging = false;
+
+
+        private Bitmap graphBitmap;
+
         public Form1()
         {
             InitializeComponent();
+
+            LineChart chart = new LineChart(pictureBox1);
+
+            chart.Draw(this.Font);
+
+
+
         }
+
+        
+
     }
 }
